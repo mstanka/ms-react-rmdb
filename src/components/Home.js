666afa5ca812ab9ce0dd4216ad8a75e8262f74bd
mstanka <mice78.mm@gmail.com>
2020-07-30 22:18:19 +0200

    add home and header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE,
+} from "../config";
 
 // import components
 import HeroImage from "./elements/HeroImage";
@@ -8,15 +15,31 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
-const Home = () => (
-  <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <Spinner />
-    <LoadMoreBtn />
-  </>
-);
+// import custom hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
+const Home = () => {
+
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
+
+  if(error) return <div>Something went wrong</div>
+if(!state.movies[0]) return <Spinner />
+
+  return (
+    <>
+      <HeroImage 
+      image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+      title={state.heroImage.original_title}
+      text={state.heroImage.overview}
+      />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
+    </>
+  );
+};
 
 export default Home;
